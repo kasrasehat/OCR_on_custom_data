76e4dbcc47a36a15cb69791998eb31a5547febd9
kasra <kasra.sehat@yahoo.com>
2023-11-08 00:16:47 +0330

    create csv for all data. In order to pass data to model altogether

--- create_csv_all.py
+++ create_csv_all.py
@@ -20,7 +20,7 @@ def replace_tokens(passage):
 def create_csv_if_not_exists(csv_path):
     if not os.path.exists(csv_path):
         # Create an empty DataFrame with the same columns as BB
-        df = pd.DataFrame(columns=['location'])
+        df = pd.DataFrame(columns=['location','csv_file'])
 
         # Save the empty DataFrame to a new CSV file named "AAA.csv"
         df.to_csv(csv_path, encoding='UTF-8-SIG', index=False)
@@ -55,6 +55,9 @@ trans = transforms.Compose([
     transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])])
 
 
+csv_path = '/home/kasra/kasra_files/data-shenasname/data_loc'
+create_csv_if_not_exists(csv_path)
+new_csv = pd.read_csv(csv_path, encoding='UTF-8-SIG')
 files = glob.glob('/home/kasra/kasra_files/data-shenasname/*.CSV') + glob.glob('/home/kasra/kasra_files/data-shenasname/*.csv')
 file_list = []
 for file in files:
@@ -65,36 +68,10 @@ min = 100
 p = 0
 for index, file in tqdm.tqdm(enumerate(file_list)):
 
-    dataset1 = ID_card_DataLoader(image_folder=file[1], label_file=file[0], transform=trans)
-    dataloader1 = DataLoader(dataset1, batch_size=batch_size, shuffle=True, drop_last=True)
+    for i, image_file in enumerate(os.listdir(file[1])):
+        new_csv.loc[p, 'location'] = file[1] + f'/{image_file}'
+        new_csv.loc[p, 'csv_file'] = file[0]
+        p += 1
 
-    # Iterate through the DataLoader to get one dictionary for each data point
-    for batch in dataloader1:
-        images, labels = batch  # Unpack the batch into images and labels
-        # for i in range(len(images)):
-        #     image = images[i]
-        #     label = labels['passage'][i]
-            # Process the image and label as needed
-            # 'image' and 'label' are now one dictionary for each data point in the batch
-#     for i, data in tqdm.tqdm(enumerate(dataloader1)):
-#         print(i)
-#         print(data[0].shape)
-#         for p in range(16):
-#             if len(list(data[1])[p]) > max:
-#                 max = len(list(data[1])[p])
-#
-#         for j in range(16):
-#             if len(list(data[1])[j]) < min:
-#                 min = len(list(data[1])[j])
-#
-#         for k in range(len(list(data[1]))):
-#             for token in list(list(data[1])[k]):
-#                 if token not in tokens:
-#                     tokens.append(token)
-#
-#
-#
-# print(max)
-# print(min)
-# with open("tokens_list.pkl", "wb") as f:
-#     pickle.dump(tokens, f)
+
+new_csv.to_csv(csv_path, encoding='UTF-8-SIG', index=False)
